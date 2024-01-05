b8da5e195cffb0daea00e1eb0bd40bbb71d12553
Rohit Gandhi <rohitgupta1312000@gmail.com>
2024-01-05 15:19:38 +0530

    done

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -21,5 +21,21 @@ namespace BulkyWeb.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Category obj)// when we hit create button after filling form then it create a post request that why we are using httppost
+        {  
+            if(obj.Name==obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name" ,"DisplayOrder can not match exactly the same");
+            }
+            if (ModelState.IsValid)
+             {
+                _Db.Categories.Add(obj);
+                _Db.SaveChanges(); // save all the changes made in this context to database
+                return RedirectToAction("Index"); //it will redirect to action name  index  and execute the code inside that
+             }
+          return View();
+           
+        }
     }
 }
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -7,11 +7,13 @@ namespace BulkyWeb.Models
     {
         [Key]                                   // this is data annotation for primary key;
         public int Id { get; set; }            // this will be our primary key
-
-        [Required]                             // `[Required]` attribute is used to specify that a property or parameter is required and must have a value
+                                               // `[Required]` attribute is used to specify that a property or parameter is required and must have a value
+        [Required]
+        [MaxLength(30)]
         [DisplayName("Category Name")]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1,1000,ErrorMessage ="Display Order must be between 1-1000.")]
         public int DisplayOrder { get; set; }
     }
 }
