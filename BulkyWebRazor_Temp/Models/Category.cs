da5f3c8d300a80cf1909273d2be77d772ded19ce
Rohit Gandhi <rohitgupta1312000@gmail.com>
2024-01-08 22:48:35 +0530

    all changes of Razor Project

---
+++ BulkyWebRazor_Temp/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkyWebRazor_Temp.Models
+{
+    public class Category
+    {
+
+        [Key]                                   // this is data annotation for primary key;
+        public int Id { get; set; }            // this will be our primary key
+                                               // `[Required]` attribute is used to specify that a property or parameter is required and must have a value
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 1000, ErrorMessage = "Display Order must be between 1-1000.")]
+        public int DisplayOrder { get; set; }
+    }
+}
