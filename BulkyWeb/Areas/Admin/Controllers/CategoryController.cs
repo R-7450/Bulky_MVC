117164c5c21db004ffab6d712a07b63b2c6d33ee
Rohit Gandhi <rohitgupta1312000@gmail.com>
2024-01-12 18:19:30 +0530

    till 83 lectures

--- BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -1,22 +1,25 @@
 ﻿
 using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository;
 using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BulkyWeb.Controllers
+namespace BulkyWeb.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
-        private readonly ICategoryRepository _categoryRepo;
-        public CategoryController(ICategoryRepository db)
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoryController(IUnitOfWork unitOfWork)
         {
-            _categoryRepo = db;
-            
+            _unitOfWork = unitOfWork;
+
+
         }
         public IActionResult Index()
         {
-           List<Category>  objCategoryList = _categoryRepo.GetAll().ToList(); // command is used all the records from category table
+            List<Category> objCategoryList = _unitOfWork.Category.GetAll().ToList(); // command is used all the records from category table
             return View(objCategoryList);
         }
         public IActionResult Create()
@@ -32,24 +35,24 @@ namespace BulkyWeb.Controllers
             }
 
             if (ModelState.IsValid)
-             {
-                _categoryRepo.Add(obj);
-                _categoryRepo.Save(); // save all the changes made in this context to database
+            {
+                _unitOfWork.Category.Add(obj);
+                _unitOfWork.Save(); // save all the changes made in this context to database
                 TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index"); //it will redirect to action name  index  and execute the code inside that
-             }
-          return View();
-           
+            }
+            return View();
+
         }
         public IActionResult Edit(int? id)  // if something is not mention by default this will be Get  Action
         {
-            if(id==null||id==0)
+            if (id == null || id == 0)
             {
                 return NotFound();
             }
-            Category? categoryFromDb = _categoryRepo.Get(u=>u.Id==id); // find method work on only primary key only
+            Category? categoryFromDb = _unitOfWork.Category.Get(u => u.Id == id); // find method work on only primary key only
             //Category? categoryFromDb1 = _Db.Categories.FirstOrDefault(c => c.Id == id);// this is another method to find category list
-            if(categoryFromDb==null)
+            if (categoryFromDb == null)
             {
                 return NotFound();
             }
@@ -58,11 +61,11 @@ namespace BulkyWeb.Controllers
         [HttpPost]    // below Edit Action is Post Action
         public IActionResult Edit(Category obj)// when we hit create button after filling form then it create a post request that why we are using httppost
         {
-       
+
             if (ModelState.IsValid)
             {
-                _categoryRepo.Update(obj);
-                _categoryRepo.Save(); // save all the changes made in this context to database
+                _unitOfWork.Category.Update(obj);
+                _unitOfWork.Save(); // save all the changes made in this context to database
                 TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index"); //it will redirect to action name  index  and execute the code inside that
             }
@@ -75,7 +78,7 @@ namespace BulkyWeb.Controllers
             {
                 return NotFound();
             }
-            Category? categoryFromDb = _categoryRepo.Get(u => u.Id == id); // find method work on only primary key only
+            Category? categoryFromDb = _unitOfWork.Category.Get(u => u.Id == id); // find method work on only primary key only
             //Category? categoryFromDb1 = _Db.Categories.FirstOrDefault(c => c.Id == id);// this is another method to find category list
             if (categoryFromDb == null)
             {
@@ -83,18 +86,18 @@ namespace BulkyWeb.Controllers
             }
             return View(categoryFromDb);
         }
-        [HttpPost,ActionName("Delete")]  
-        public IActionResult DeletePost(int? id )// when we hit create button after filling form then it create a post request that why we are using httppost
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)// when we hit create button after filling form then it create a post request that why we are using httppost
         {
-            Category? obj= _categoryRepo.Get(u => u.Id == id);// since both the method have same name thats why we name this method as deletepost
-            if(obj==null)
+            Category? obj = _unitOfWork.Category.Get(u => u.Id == id);// since both the method have same name thats why we name this method as deletepost
+            if (obj == null)
             {
                 return NotFound();
             }
-            _categoryRepo.Remove(obj);
-            _categoryRepo.Save();
+            _unitOfWork.Category.Remove(obj);
+            _unitOfWork.Save();
             TempData["success"] = "Category deleted successfully";
-            return RedirectToAction("Index");    
+            return RedirectToAction("Index");
 
         }
     }
