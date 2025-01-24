e93ffebbb537b3c2ffc111171fc1b00c1d7dd0e1
R-7450 <rohitgupta1312000@gmail.com>
2025-01-24 10:54:21 +0530

    Display company dropdown

--- BulkyWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BulkyWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -10,6 +10,8 @@ using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
+using Bulky.DataAccess.Repository;
+using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
 using Bulky.Utility;
 using Microsoft.AspNetCore.Authentication;
@@ -34,6 +36,7 @@ namespace BulkyWeb.Areas.Identity.Pages.Account
         private readonly IUserEmailStore<IdentityUser> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender; //IEmailSender is a dependency which implementarion we have provided in Bulky.Utility folder and services in Program.cs file
+        private readonly IUnitOfWork _unitOfWork;
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
@@ -41,8 +44,10 @@ namespace BulkyWeb.Areas.Identity.Pages.Account
             IUserStore<IdentityUser> userStore,
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            IUnitOfWork unitOfWork)
         {
+            _unitOfWork = unitOfWork;
             _userManager = userManager;
             _roleManager = roleManager;
             _userStore = userStore;
@@ -118,6 +123,10 @@ namespace BulkyWeb.Areas.Identity.Pages.Account
 
             public string PhoneNumber { get; set; }
 
+            public int? CompanyId { get; set; }
+            [ValidateNever]
+            public IEnumerable<SelectListItem> CompanyList { get; set; }
+
         }
 
 
@@ -137,8 +146,13 @@ namespace BulkyWeb.Areas.Identity.Pages.Account
                 {
                     Text = i,
                     Value = i
-                })
-            };
+                }),
+                 CompanyList = _unitOfWork.Company.GetAll().Select(i => new SelectListItem
+                 {
+                     Text = i.Name,
+                     Value = i.Id.ToString()
+                 })
+            }; 
 
            
         
