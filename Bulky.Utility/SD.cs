9561e5c298dbd8701edc1f1775b15e0de4c4e185
R-7450 <rohitgupta1312000@gmail.com>
2025-01-24 10:20:47 +0530

    completed lecture 126, added company crud

--- Bulky.Utility/SD.cs
+++ Bulky.Utility/SD.cs
@@ -8,7 +8,7 @@ namespace Bulky.Utility
 {
     public static class SD     // static details
     {
-        public const string Role_Customer = "Customer";
+        public const string Role_Customer = "Customer"; // if a user role is customer then they will have advantage to make payment for order under 30 days.
         public const string Role_Company = "Company";
         public const string Role_Admin = "Admin";
         public  const string Role_Employee = "Employee";
