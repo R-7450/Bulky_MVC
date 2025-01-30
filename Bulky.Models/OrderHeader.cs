21edfd5eedb1a2fb84d573d7a0b9d78812573e4c
R-7450 <rohitgupta1312000@gmail.com>
2025-01-30 12:28:26 +0530

    completed lecture 143

---
+++ Bulky.Models/OrderHeader.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.Models
+{
+    public class OrderHeader
+    {
+        public int Id { get; set; }
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        [ValidateNever]
+
+        public ApplicationUser ApplicationUser { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime ShippingDate { get; set; }
+        public double OrderTotal { get; set; }
+
+        public string? OrderStatus { get; set; }
+        public string? PaymentStatus { get; set; }
+        public string? TrackingNumber { get; set; }
+        public string? Carrier { get; set; }    
+        public DateTime PaymentDate { get; set; }
+        public DateOnly PaymentDueDate { get; set; } // dateonly is a feature of .NET version 8.
+
+        public string? PaymentIntentId { get; set; } // when we done transaction using credit card or stripe then unique id will be there which will be our PaymentIntentId
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public string StreetAddress { get; set; }
+        [Required]
+        public string PostalCode { get; set; }
+
+    }
+}
