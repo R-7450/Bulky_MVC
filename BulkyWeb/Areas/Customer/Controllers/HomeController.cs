c245b95a0b002267a906f848c7c5115086f029da
R-7450 <rohitgupta1312000@gmail.com>
2025-02-01 16:04:57 +0530

    completed lecture 174 and 175

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -24,6 +24,12 @@ namespace BulkyWeb.Areas.Customer.Controllers
 
         public IActionResult Index()
         {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim= claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            if(claim!=null)
+            {
+                HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value).Count());
+            }
             IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
 
             return View(productList);//if nothing is present inside view bracket then it will go to index file inside home folder
