6cf412201761895f929d9a1c66a0d4c004461b48
R-7450 <rohitgupta1312000@gmail.com>
2025-02-01 16:38:25 +0530

    completed lecture176 and 177

--- BulkyWeb/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ BulkyWeb/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -26,7 +26,7 @@ namespace BulkyWeb.Areas.Identity.Pages.Account
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
-            HttpContext.Session.Clear();// if a user is logged out then number of items count will set to zero on  UI main page
+          
             _logger.LogInformation("User logged out.");
             if (returnUrl != null)
             {
